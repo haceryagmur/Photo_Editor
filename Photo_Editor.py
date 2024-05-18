b7b6b2a912ed8f326984fab2ce5b14f1b2fb9edb
Hacer Yagmur <haceryagmur2004@gmail.com>
2024-05-18 18:03:42 +0200

    Add files via upload

---
+++ Photo_Editor.py
@@ -0,0 +1,21 @@
+# Photo Editor
+
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = "C:/Users/hp/Desktop/imgs"
+pathOut = "C:/Users/hp/Desktop/editedimgs"
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    
+    edit = img.filter(ImageFilter.SHARPEN()).convert("L").rotate(-90)
+    
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+    
+    clean_name = os.path.splitext(filename)[0]
+    
+    edit.save(f"{pathOut}/{clean_name}_edited.jpg")
+    
